4e67279926040449c59283cf0cb484caf247bb82
maitree11 <maitreekandpal@gmail.com>
2026-03-28 08:43:08 +0530

    count substring occurrence

---
+++ exp 4/exp4_4.py
@@ -0,0 +1,9 @@
+string = input("Enter main string: ")
+substring = input("Enter substring: ")
+
+count = 0
+for i in range (len(string) - len(substring) + 1):
+    if string[i:i+len(substring)] == substring:
+        count += 1
+
+print(count)
