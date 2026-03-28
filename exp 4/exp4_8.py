28fff96ebf53ad42f260b75715bf627122515c29
maitree11 <maitreekandpal@gmail.com>
2026-03-28 08:44:31 +0530

    pre defined set operations

---
+++ exp 4/exp4_8.py
@@ -0,0 +1,7 @@
+s1 = {"red", "yellow", "orange", "blue"}
+s2 = {"violet", "blue", "purple"}
+
+print("Union: ", s1 | s2)
+print("Intersection: ", s1 & s2)
+print("Difference s1-s2: ", s1 - s2)
+print("Difference s2-s1", s2 - s1)
