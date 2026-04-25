75bd4f4703c97b06b4bbbf13bac8bc8365a112bc
maitree11 <maitreekandpal@gmail.com>
2026-04-25 14:49:48 +0530

    method overriding

---
+++ exp 9/exp9_4.py
@@ -0,0 +1,20 @@
+class Animal:
+    def sound(self):
+        print("Animals make sound")
+
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+
+class Cat(Animal):
+    def sound(self):
+        print("Cat meows")
+
+
+d = Dog()
+c = Cat()
+
+d.sound()
+c.sound()
